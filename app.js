c26b37bea5ce2ef1c0f739f7a5a762965ea658f5
Mohamed-Hossam30 <mohamedhossamaugust@gmail.com>
2025-08-04 09:21:03 +0300

    New edit

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const express = require ('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname});
-})
+app.use(express.json());
 
-app.listen(port, () => {
+const Mydata = require("./models/myDataSchema")
+
+
+app.get("/",(req,res) =>
+{
+   res.sendFile("./views/home.html", {root: __dirname});
+}
+);
+
+app.get("/index.html",(req,res) =>
+{
+   res.send("<h1>Data sent successfully</h1>")
+}
+);
+
+
+mongoose.connect("mongodb+srv://Mohamed_Hossam:2MsyAKapB4hYsGOt@cluster0.388xnxa.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+  app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
 })
+})
+.catch((err)=>{console.log(err)})
+;
+
+app.post("/", (req, res) => {
+  console.log(req.body)
+
+  const mydata = new Mydata(req.body)
+
+  mydata.save().then(()=> 
+  {
+      res.redirect("/index.html")
+  }).catch((err)=>{
+    console.log(err)
+  });
+})
+
+
+
